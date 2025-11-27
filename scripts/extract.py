f4e1e93c7e6e413cc21eae6f9ecced2790a9e154
aboywanttocode <159906352+aboywanttocode@users.noreply.github.com>
2025-11-27 18:38:24 +0200

    Add extract_weather_history task for Kaggle dataset
    This function downloads a dataset from Kaggle, unzips it, and returns the path of the extracted CSV file.

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,14 +1,42 @@
 import os
 import zipfile
+import pandas as pd
 from kaggle.api.kaggle_api_extended import KaggleApi
+from airflow.decorators import task
 
-def download_kaggle_dataset():
-    os.environ['KAGGLE_CONFIG_DIR'] = os.path.expanduser('~/.kaggle')
+@task
+def extract_weather_history():
+    """
+    Extract Task:
+    - Download dataset from Kaggle
+    - unzip file ZIP
+    - return file CSV path through XCom
+    """
+    # 1. Kaggle API Authentication
+    os.environ["KAGGLE_CONFIG_DIR"] = os.path.expanduser("~/.kaggle")
     api = KaggleApi()
     api.authenticate()
-    api.dataset_download_files('username/dataset-name', path='data/', unzip=False)
 
+    # 2. Download ZIP dataset
+    zip_output_dir = "data/"
+    zip_file_path = os.path.join(zip_output_dir, "weather_history.zip")
 
-def unzip_file(zip_path, extract_to='data/'):
-    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-        zip_ref.extractall(extract_to)
+    api.dataset_download_files(
+        "username/dataset-name",
+        path=zip_output_dir,
+        unzip=False
+    )
+
+    # 3. Unzip file
+    with zipfile.ZipFile(zip_file_path, "r") as zip_ref:
+        zip_ref.extractall(zip_output_dir)
+
+    # 4. 
+    csv_files = [f for f in os.listdir(zip_output_dir) if f.endswith(".csv")]
+    if not csv_files:
+        raise FileNotFoundError("No CSV file found after extraction!")
+
+    csv_path = os.path.join(zip_output_dir, csv_files[0])
+
+    # 5. Pass file path to next task 
+    return csv_path
